71db7daa793c10c38a4d765f019ecd39e27ec92d
victorbiguete <victorbiguete@gmail.com>
2025-07-29 21:24:45 -0300

    feat: implementando Fluent Migration

---
+++ src/Backend/MyRecepiBook.Infrastructure/Migration/DatabaseVersions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.Migration
+{
+    public abstract record DatabaseVersions
+    {
+        public const int TABLE_USER = 1;
+    }
+}
---
+++ src/Backend/MyRecepiBook.Infrastructure/Migration/Versions/Version000001.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using FluentMigrator.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.Migration.Versions
+{
+    [Migration(DatabaseVersions.TABLE_USER,"Create table to save the User's information")]
+    public class Version000001 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
