36f7d8d6469ca74510555069b00baf8d37d5525f
victorbiguete <victorbiguete@gmail.com>
2025-07-29 21:34:46 -0300

    feat: Implementando a Função Up para Criar a Tabela Usuario com FluentMigrator

--- src/Backend/MyRecepiBook.Infrastructure/Migration/Versions/Version000001.cs
+++ src/Backend/MyRecepiBook.Infrastructure/Migration/Versions/Version000001.cs
@@ -10,11 +10,14 @@ using System.Threading.Tasks;
 namespace MyRecipeBook.Infrastructure.Migration.Versions
 {
     [Migration(DatabaseVersions.TABLE_USER,"Create table to save the User's information")]
-    public class Version000001 : ForwardOnlyMigration
+    public class Version000001 : VersionBase
     {
         public override void Up()
         {
-            throw new NotImplementedException();
+                CreateTable("Users")
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Email").AsString(255).NotNullable()
+                .WithColumn("Password").AsString(2000).NotNullable();
         }
     }
 }
---
+++ src/Backend/MyRecepiBook.Infrastructure/Migration/Versions/VersionBase.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator.Builders.Create.Table;
+
+namespace MyRecipeBook.Infrastructure.Migration.Versions
+{
+    public abstract class VersionBase : ForwardOnlyMigration
+    {
+        protected ICreateTableColumnOptionOrWithColumnSyntax CreateTable(string table)
+        {
+            return Create.Table(table)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("CreatedOn").AsDateTime().NotNullable()
+                .WithColumn("Active").AsBoolean().NotNullable();
+        }
+    }
+}
