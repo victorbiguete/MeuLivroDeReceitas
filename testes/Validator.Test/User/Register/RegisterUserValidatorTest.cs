69b57c80fbaf2582f3e4dcac9993c6c26ec32812
victorbiguete <victorbiguete@gmail.com>
2025-07-30 21:33:47 -0300

    feat: Iniciando arquitetura de testes

---
+++ testes/Validator.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,25 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Sucess()
+        {
+            var validator = new RegisterUserValidator();
+
+            
+
+            var result = validator.Validate();
+
+            result.IsValid == true
+        }
+    }
+}
