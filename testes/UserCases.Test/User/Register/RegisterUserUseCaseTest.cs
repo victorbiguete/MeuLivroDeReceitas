103ac6d3e6353caf4001427e6cfae6f7521a74a2
victorbiguete <victorbiguete@gmail.com>
2025-08-01 22:34:32 -0300

    feat: Iniciando Testes de Unidade para o Use Case

---
+++ testes/UserCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,28 @@
+﻿using CommomTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.User.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCases.Test.User.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        public async Task Success()
+        {
+            var useCase = new RegisterUserUseCase();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var result = await useCase.Execute(request);
+
+            result.Name.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+
+            
+        }
+    }
+}
