3be43bffa3cb97b70e9203bc42425cd31cb91869
victorbiguete <victorbiguete@gmail.com>
2025-08-06 20:22:47 -0300

    feat: Configurando Banco de Dados In-Memory para Testes de Integração

---
+++ testes/WebApi.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Test
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test")
+                .ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                    if(descriptor is not null)
+                        services.Remove(descriptor);
+
+                    var provider = services
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                    services.AddDbContext<AppDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+                        options.UseInternalServiceProvider(provider);
+                    });
+                });
+        }
+    }
+}
--- testes/WebApi.Test/User/Register/RegisterUserTest.cs
+++ testes/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -7,10 +7,10 @@ using System.Text.Json;
 
 namespace WebApi.Test.User.Register
 {
-    public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+    public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
     {
         private readonly HttpClient _httpClient;
-        public RegisterUserTest(WebApplicationFactory<Program> factory)
+        public RegisterUserTest(CustomWebApplicationFactory factory)
         {
             _httpClient = factory.CreateClient();
         }
