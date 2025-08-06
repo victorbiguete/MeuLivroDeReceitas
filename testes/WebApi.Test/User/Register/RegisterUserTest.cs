311bd42084735a7bce23ba8602f893f71325a8ae
victorbiguete <victorbiguete@gmail.com>
2025-08-06 20:07:18 -0300

    feat: Teste de Integração Completo para o Caso de Sucesso

--- testes/WebApi.Test/User/Register/RegisterUserTest.cs
+++ testes/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -1,6 +1,9 @@
 ﻿using CommomTestsUtilities.Requests;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace WebApi.Test.User.Register
 {
@@ -17,7 +20,19 @@ namespace WebApi.Test.User.Register
         {
             var request = RequestRegisterUserJsonBuilder.Build();
 
-            await _httpClient.PostAsJsonAsync("User",request);
+            var response = await _httpClient.PostAsJsonAsync("/api/User",request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("name")
+                .GetString()
+                .Should()
+                .NotBeNullOrWhiteSpace()
+                .And.Be(request.Name);
         }
     }
 }
