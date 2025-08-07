e615c4a35eadf52c0c0e5e332220eebf90b908cf
victorbiguete <victorbiguete@gmail.com>
2025-08-06 21:04:24 -0300

    feat:Testando Erros em Cenários de Integração

---
+++ testes/WebApi.Test/InlineData/CultureInlineDataTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Test.InlineData
+{
+    public class CultureInlineDataTest : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]{ "en" };
+            yield return new object[]{ "pt-BR" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- testes/WebApi.Test/User/Register/RegisterUserTest.cs
+++ testes/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register
 {
@@ -33,6 +34,34 @@ namespace WebApi.Test.User.Register
                 .Should()
                 .NotBeNullOrWhiteSpace()
                 .And.Be(request.Name);
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string culture)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            request.Name = string.Empty;
+
+            if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+                _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+            _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+            var response = await _httpClient.PostAsJsonAsync("/api/User", request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            errors.Should()
+                .ContainSingle()
+                .And.Contain(error => error.GetString().Equals(""));
         }
     }
 }
