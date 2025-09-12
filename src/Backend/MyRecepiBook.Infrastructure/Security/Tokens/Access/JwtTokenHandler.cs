743cad85a0b030098942a4305f70217bea574feb
victorbiguete <victorbiguete@gmail.com>
2025-09-11 22:31:15 -0300

    Fix: Avaliando e Corrigindo Métricas no SonarCloud

--- src/Backend/MyRecepiBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
+++ src/Backend/MyRecepiBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -9,7 +9,7 @@ namespace MyRecipeBook.Infrastructure.Security.Tokens.Access
 {
     public abstract class JwtTokenHandler
     {
-        protected SymmetricSecurityKey SecurityKey(string signingKey)
+        protected static SymmetricSecurityKey SecurityKey(string signingKey)
         {
             var bytes = Encoding.UTF8.GetBytes(signingKey);
             return new SymmetricSecurityKey(bytes);
--- src/Shared/MyRecipeBook.Communication/Response/ResponseErrorJson.cs
+++ src/Shared/MyRecipeBook.Communication/Response/ResponseErrorJson.cs
@@ -9,14 +9,12 @@ namespace MyRecipeBook.Communication.Response
     public class ResponseErrorJson
     {
         public IList<string> Errors { get; set; }
+        public bool TokenIsExpired { get; set; }
 
         public ResponseErrorJson(IList<string> errors)
         {
             Errors = errors;
         }
-
-        public bool TokenIsExpired { get; set; }
-
         public ResponseErrorJson(string error)
         {
             //Errors = [error];
