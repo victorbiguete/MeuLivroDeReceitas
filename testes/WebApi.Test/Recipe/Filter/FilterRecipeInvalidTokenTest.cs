b16e7655f94d4e667050b1950ef7ba112cd7872e
victorbiguete <victorbiguete@gmail.com>
2025-10-13 21:04:46 -0300

    feat: Teste de Unidade para Tokens Invalidos na requisição de Filters

--- testes/WebApi.Test/Recipe/Filter/FilterRecipeInvalidTokenTest.cs
+++ testes/WebApi.Test/Recipe/Filter/FilterRecipeInvalidTokenTest.cs
@@ -1,35 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommomTestsUtilities.Requests;
+using CommomTestsUtilities.Tokens;
+using FluentAssertions;
+using System.Net;
 
 namespace WebApi.Test.Recipe.Filter
 {
     public class FilterRecipeInvalidTokenTest : MyRecipeBookClassFixture
     {
         private const string METHOD = "recipe/filter";
-
-        public FilterRecipeInvalidTokenTest(CustomWebApplicationFactory webApplication) : base(webApplication)
+        
+        public FilterRecipeInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory)
         {
         }
 
         [Fact]
         public async Task Error_Token_Invalid()
         {
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            var response = await DoPost(method:METHOD, request: request,token:"TokenIsInvalid");
 
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
 
         [Fact]
         public async Task Error_Without_Token()
         {
-
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            var response = await DoPost(method:METHOD,request:request,token:string.Empty);
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
 
         [Fact]
         public async Task Error_Token_With_User_NotFound()
         {
+            var request = RequestFilterRecipeJsonBuilder.Build();
+            var token = JwtTokenGeneratorBuilder.Build().Generate(new Guid());
+            var response = await DoPost(method:METHOD, request: request,token:token);
 
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
     }
 }
